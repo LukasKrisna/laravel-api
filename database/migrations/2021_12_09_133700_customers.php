f1b2005898a2608e4a51405ff4a98a8e5101be3e
Lukas Krisna <lukaskrisnaaa@gmail.com>
2021-12-12 15:03:25 +0700

    laravel-api

---
+++ database/migrations/2021_12_09_133700_customers.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Customers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('email', 120)->unique();
+            $table->string('first_name');
+            $table->string('last_name')->nullable();
+            $table->string('city')->nullable();
+            $table->string('address')->nullable();
+            $table->string('password')->nullable();
+            $table->string('token')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('customers');
+    }
+}
